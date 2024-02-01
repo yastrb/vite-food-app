3e8a55b6d13ab9c7c3c2f08a86a5c62d537e4602
Olena Yastrebtsova <uaalena202@gmail.com>
2024-02-01 12:28:27 +0200

    mtailwind installed

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+import logo from "./../../assets/images/logo.png"
+
+const Header = () => {
+  return (
+    <header>
+      <div className=" relative container">
+        <div>
+          <img src={logo} alt="logo" />
+          
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
+
+
