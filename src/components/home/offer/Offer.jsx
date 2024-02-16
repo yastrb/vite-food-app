072994af586050ab3aeceb36a77cf85239581183
Olena Yastrebtsova <uaalena202@gmail.com>
2024-02-16 12:32:24 +0200

    new commit

--- src/components/home/offer/Offer.jsx
+++ src/components/home/offer/Offer.jsx
@@ -7,6 +7,10 @@ const Offer = () => {
         <div className=" flex flex-col md:flex-row">
             <div>
                 <Heading title="Special Offer"  subtitle="GOOD FOOD, DRINKS & GREAT COMPANY."/>
+                <p className=' text-gray leading-6'>As well known and we are very busy all days advice you. advice you to call us of before arriving, so we can guarantee your seat. advice you to call us of before arriving As well known and we are very busy all days advice you</p>
+                <div className=' flex'>
+                    
+                </div>
             </div>
             <div></div>
         </div>
@@ -14,4 +18,4 @@ const Offer = () => {
   )
 }
 
-export default Offer
+export default Offer 
