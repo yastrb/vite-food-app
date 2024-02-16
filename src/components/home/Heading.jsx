53d9bcb495d009ef21d9c98cd1715d102afee381
Olena Yastrebtsova <uaalena202@gmail.com>
2024-02-16 11:22:38 +0200

    added:heading.jsx

---
+++ src/components/home/Heading.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react'
+
+const Heading = (props) => {
+  return (
+    <div className='py-8 text-center'>
+        <h3 className=' title'>{props.title}</h3>
+        <h4 className=' subtitle'>{props.subtitle}</h4>
+    </div>
+  )
+}
+
+export default Heading
