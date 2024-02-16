4474693127242f3bdc8e2206b8608404c99a0ba2
Olena Yastrebtsova <uaalena202@gmail.com>
2024-02-16 15:37:59 +0200

    fixed: offer.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react'
 
 const Button = (props) => {
-    const btnClass= ` bg-${props.color} ${props.translate} px-8 py-4 rounded-xl text-[#FFFFFF] text-sm font-semibold uppercase w-[164px]`
+    const btnClass= ` bg-[#00a850] ${props.translate} px-8 py-4 rounded-xl text-[#FFFFFF] text-sm font-semibold uppercase w-[164px]`
   return (
     <button className={btnClass} >{props.label}</button>
   )
--- src/components/home/offer/Offer.jsx
+++ src/components/home/offer/Offer.jsx
@@ -1,12 +1,13 @@
 ﻿import React from 'react'
 import Heading from '../Heading'
 import Button from '../../Button'
+import offerImg from "/assets/offer_img.png"
 
 const Offer = () => {
   return (
     <section className=' mt-[7%]'>
         <div className=" flex flex-col md:flex-row">
-            <div>
+            <div className=' flex flex-col md:w-[50%]'>
                 <Heading title="Special Offer"  subtitle="GOOD FOOD, DRINKS & GREAT COMPANY."/>
                 <p className=' text-gray leading-6'>As well known and we are very busy all days advice you. advice you to call us of before arriving, so we can guarantee your seat. advice you to call us of before arriving As well known and we are very busy all days advice you</p>
                 <div className=' flex gap-5 my-8 items-center'>
@@ -14,8 +15,8 @@ const Offer = () => {
                     <span className=' text-orange text-4xl font-bold' >$46.99 </span>
                 </div>
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className=' flex md:w-[50%] justify-center items-center'>
+              <img src={offerImg} alt="" />
             </div>
         </div>
     </section>
