dad8954b6e4e58d65bf8776d0eb3af5f1c46ebfb
Olena Yastrebtsova <uaalena202@gmail.com>
2024-02-16 13:37:00 +0200

    new commit

--- src/components/home/offer/Offer.jsx
+++ src/components/home/offer/Offer.jsx
@@ -1,5 +1,6 @@
 ﻿import React from 'react'
 import Heading from '../Heading'
+import Button from '../../Button'
 
 const Offer = () => {
   return (
@@ -8,11 +9,14 @@ const Offer = () => {
             <div>
                 <Heading title="Special Offer"  subtitle="GOOD FOOD, DRINKS & GREAT COMPANY."/>
                 <p className=' text-gray leading-6'>As well known and we are very busy all days advice you. advice you to call us of before arriving, so we can guarantee your seat. advice you to call us of before arriving As well known and we are very busy all days advice you</p>
-                <div className=' flex'>
-                    
+                <div className=' flex gap-5 my-8 items-center'>
+                    <Button label="order now" color="[#00a850]"/>
+                    <span className=' text-orange text-4xl font-bold' >$46.99 </span>
                 </div>
             </div>
-            <div></div>
+            <div>
+              <img src="" alt="" />
+            </div>
         </div>
     </section>
   )
