8e35936e8d5f2bbb55c8f292629b42c8acdc4edb
Olena Yastrebtsova <uaalena202@gmail.com>
2024-02-20 13:39:03 +0200

    padding removed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,10 @@
 ﻿import React from 'react'
-import Hero from './hero/Hero'
 import Discount from './discount/discount'
 
 const Home = () => {
   return (
-    <main className='mx-auto px-4 mt-[80px]'>
-      <Hero/>
+    <main className='mx-auto mt-[80px]'>
+
       <Discount/>
     </main>
   )
--- src/components/home/discount/Discount.jsx
+++ src/components/home/discount/Discount.jsx
@@ -1,8 +1,18 @@
 ﻿import React from 'react'
+import bg from "/image/offer_bg.jpg"
+import Heading from '../Heading'
+
 
 const Discount = () => {
   return (
-    <section>discount</section>
+    <section className=' mt-[7%] relative'>
+        <div className=' absolute -z-10'>
+            <img src={bg} alt="" />
+        </div>
+        <div className=' grid'>
+            <Heading title='Special Kombo Pack' subtitle="WE MAKE THE BEST * BURGER IN YOUR TOWN"/>
+        </div>
+    </section>
   )
 }
 
