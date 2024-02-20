e6e9c0ff85a53083e899a206912b95f30e9f2495
Olena Yastrebtsova <uaalena202@gmail.com>
2024-02-20 13:09:18 +0200

    added: popular section

--- src/components/home/popular/Popular.jsx
+++ src/components/home/popular/Popular.jsx
@@ -1,12 +1,32 @@
 ﻿import React from 'react'
 import Heading from './../Heading';
+// import Button from './../Button';
+import {dishes} from '../../../data/Data.jsx'
+import Dishes from './Dishes';
 
 const Popular = () => {
   return (
-    <section className=' mt-[7%]'>
+    <section className=' mt-[7%] lg:w-[80%] m-auto'>
         <Heading title="Popular dishes" subtitle="POPULAR DISHES"/>
-        <div className=' flex flex-wrap'>
-            
+        {/* buttons */}
+        <div className=' flex flex-wrap '>
+            {/* <Button color="orange" label="ALL"/> */}
+          <button>All</button>
+        </div>
+        {/* dishes */}
+        <div className=' grid gap-8 grid-cols-2 lg:grid-cols-3'>
+          {dishes.map((item)=> (
+            <Dishes key={item.id}
+            id={item.id}
+            title={item.title}
+            alt={item.title}
+            category={item.category}
+            subcategory={item.sub}
+            cover={item.cover}
+            price={item.price}
+            rate={item.rate}
+            />
+          ))}
         </div>
     </section>
   )
